e5bb75c4e6ef5c0cbcd64f098ca717e752215117
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-12-11 17:25:41 -0500

    Paddle sprite and Player class

--- lib/game.py
+++ lib/game.py
@@ -1,15 +1,21 @@
 import pygame
 
 from lib.constants import *
+from lib.sprites import *
 
 class Game():
     def __init__(self):
         # initialize
         pygame.init()
         self.running = True
-        self.display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Pong")
         self.clock = pygame.time.Clock()
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+        self.paddle_sprites = pygame.sprite.Group()
+        self.player = Player((self.all_sprites, self.paddle_sprites))
 
     def run(self):
         while self.running:
@@ -22,7 +28,10 @@ class Game():
                     self.running = False
 
             # update
+            self.all_sprites.update(dt)
+
             # draw
-            self.display_surface.fill("black")
+            self.display_surface.fill(COLORS["bg"])
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
         pygame.quit()
